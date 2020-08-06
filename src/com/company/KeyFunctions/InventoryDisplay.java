9ec21ab4439c0e58a712c2ad51b01aacc876d1b5
LFCRab <56073739+LFCRab@users.noreply.github.com>
2020-08-06 16:41:05 +0100

    :sparkles: further story progression and gender assignment added :sparkles:

--- src/com/company/KeyFunctions/InventoryDisplay.java
+++ src/com/company/KeyFunctions/InventoryDisplay.java
@@ -4,17 +4,18 @@ import java.util.HashMap;
 
 public class InventoryDisplay {
     // Converts The items in the inventory into a readable format
-    public static String inventoryToString(HashMap<String, Integer> arr){
+    public static String inventoryToString(HashMap<String, Integer> arr, String connectorType){
         StringBuilder returnValue = new StringBuilder();
         String punctuation = ",";
         int index = 0;
         for (String i : arr.keySet()) {
             index++;
-            String item = String.format("%d %s%s",arr.get(i), i, punctuation);
+            String item = String.format("%d %s%s ",arr.get(i), i, punctuation);
             returnValue.append(item);
             if (index == arr.size()-1)
             {
-                returnValue.append(" and ");
+                String connection = String.format(" %s ",connectorType);
+                returnValue.append(connection);
                 punctuation = "";
             }
         }
